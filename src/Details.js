279304c72f3ee68a0bdebd737e201e832d8fbc91
Abmstpha <21007@esp.mr>
2024-05-08 12:49:27 +0000

    My portfolio

---
+++ src/Details.js
@@ -0,0 +1,273 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/rimai.jpg";
+import logo from "./assets/rimai.jpg";
+// Profile Image
+import profile from "./assets/pro.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import python from './assets/techstack/python_logo.jpg';
+import R from './assets/techstack/r_logo.png';
+import powerbi from './assets/techstack/powerbi_logo.png';
+import jupyter from './assets/techstack/jupyter_logo.png';
+
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.jpeg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Abdellahi",
+  tagline: "I am a Data Enthusiast",
+  img: profile,
+  about: `I'm passionate about Data Science, Economics, and Statistics, with a focus on applying analytical techniques to solve real-world problems. With a strong foundation in statistical analysis and AI, I excel in deriving valuable insights from complex datasets. My dedication to continuous learning and collaborative teamwork ensures precise and innovative solutions that drive meaningful impact.`,
+};
+
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/abdellahi-el-moustapha/",
+  github: "https://github.com/Abmstpha",
+  // twitter: "https://twitter.com/",
+  // instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Data Engineering Internship",
+    Company: "The Central Bank of Mauritania",
+    Location: "Nouakchott/Mauritania",
+    Type: "Internship",
+    Duration: "May 2024 - Present",
+    Responsibilities: [
+      "Automating the extraction process of submission reports from primary banks’ data.",
+      "Developing an application using Django and React frameworks and using data visualization techniques to unveil valuable insights from the data of primary banks."
+    ]
+  },
+  {
+    Position: "Data Science Internship",
+    Company: "DataTuring",
+    Location: "Remote-Nouakchott/Mauritania",
+    Type: "Internship",
+    Duration: "May 2023 - Sept 2023",
+    Responsibilities: [
+      "Conducted data collection surveys, employing both online and on-site methodologies.",
+      "Leveraged statistical analysis techniques to examine data thoroughly and applied XAI methodologies.",
+      "Developed interactive data dashboards for efficient data visualization and presentation."
+    ]
+  },
+  {
+    Position: "Data Science Internship",
+    Company: "National Agency of Statistics, Demographic and Economic Analysis",
+    Location: "Nouakchott/Mauritania",
+    Type: "Internship",
+    Duration: "Dec 2022 - May 2023",
+    Responsibilities: [
+      "Constructed a predictive model for assessing anemia probability.",
+      "Implemented efficient data preprocessing techniques."
+    ]
+  },
+  {
+    Position: "Discovery Internship",
+    Company: "National Social Security Fund",
+    Location: "Nouakchott/Mauritania",
+    Type: "Internship",
+    Duration: "Aug 2022 - Sept 2022",
+    Responsibilities: [
+      "Conducted GDP forecasting for Mauritania using statistical and economic modeling techniques.",
+      "Contributed to the development of a local ILO pension model, applying economic analysis."
+    ]
+  }
+];
+
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Data Science Engineering Diploma",
+    Company: "Higher Polytechnic School (ESP)",
+    Location: "Expected 2025",
+    Type: "Diploma",
+    Duration: "Data Science Engineering Diploma, Higher Polytechnic School (ESP) Expected 2025",
+    Responsibilities: [
+      "Focused on proficiency in statistics, Economics, data science, and project management.",
+      "Engaged in extracurricular data science projects and competitions to enhance skills."
+    ]
+  },
+  {
+    Position: "Two Years Of Intensive Math And Physics Courses ",
+    Company: "Preparatory Classes for Engineering Grandes ´Ecoles (CPGEI)",
+    Location: "2019 - 2021",
+    Type: "Non-Degree Program",
+    Duration: "Preparatory Classes for Engineering Grandes ´Ecoles (CPGEI) 2019 - 2021",
+    Responsibilities: [
+      "Completed an intensive two-year program, focusing on mathematics, physics, and engineering sciences.",
+      "Developed strong problem-solving, working under pressure, and effective time management skills."
+    ]
+  }
+];
+
+
+export const extraCurricularActivities = [
+  {
+    name: "ESP Data Club (CLEF-AUF Member)",
+    role: "Founder",
+    duration: "05/2022 - Present"
+  },
+  {
+    name: "Mauritanian AI Community",
+    role: "Executive Board Member",
+    duration: "01/2024 - Present"
+  },
+  {
+    name: "Global AI Hub",
+    role: "Community Lead",
+    duration: "04/2022 - Present"
+  }
+];
+
+export const achievements = [
+  {
+    title: "I2COMSAPP-2024 International Conference on Artificial Intelligence Organizer",
+    description: "Played a pivotal role in organizing the International Conference on Artificial Intelligence in Mauritania, contributing to the advancement of AI research and networking within the global community."
+  },
+  {
+    title: "One Million Leaders (OML-MENA-2024) Fellow",
+    description: "Acknowledged as a potential leader within the MENA region, demonstrating leadership qualities and potential."
+  },
+  {
+    title: "ESP Data Challenge 2023: Best Notebook Prize Winner",
+    description: "Recognized for exceptional performance and innovative solutions in a Kaggle competition."
+  },
+  {
+    title: "Harvard Aspire Leadership 2023 Alumnus",
+    description: "Completed the prestigious Aspire Leaders program, honing leadership skills and fostering professional growth, and received a certificate of completion along with a letter of recommendation from the prestigious Aspire institute in Harvard."
+  },
+  {
+    title: "B Global Talent Business Challenge Candidate 2023",
+    description: "Selected as a candidate for the B Global Talent Business Challenge in Spain, representing my country Mauritania and showcasing business acumen and potential."
+  },
+  {
+    title: "AfriCanarias Entrepreneurship Challenge Candidate 2022",
+    description: "Nominated as a candidate for the AfriCanarias Entrepreneurship program in the Canary Islands, representing my country Mauritania and reflecting entrepreneurial spirit and ambition."
+  },
+  {
+    title: "Mauritanian Engineering Community Forum 2022 Organizer",
+    description: "Played a key role in organizing the Mauritanian Engineering Community Forum 2022, facilitating knowledge exchange and networking opportunities."
+  },
+  {
+    title: "YOUNGA Summit 2022 Delegate",
+    description: "Represented at the YOUNGA Summit 2022, engaging in discussions and initiatives focused on global youth empowerment."
+  },
+  {
+    title: "YOUTEX Future Leaders 2022 Participant",
+    description: "Engaged as a participant in the YOUTEX Future Leaders program, demonstrating potential for future leadership roles."
+  },
+  {
+    title: "GDSC-ISCAE (2021-2022) Core Team Member",
+    description: "Contributed as a core team member of Google Developers Club in the ISCAE Institute, actively involved in organizing events and initiatives in the English language."
+  },
+  {
+    title: "African Debate Championship 2021 Candidate",
+    description: "Selected as a candidate for the African Debate Championship, showcasing communication and critical thinking skills."
+  }
+];
+
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  python: python, 
+  R: R, 
+  powerbi: powerbi, 
+  jupyter: jupyter, 
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+
+};
+
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "AFRICAN CUP OF NATION PREDICTION",
+    image: projectImage1,
+    description: `Predictions for the African Cup of Nations.`,
+    techstack: "Data Science",
+    githubLink: "https://github.com/Abmstpha/My_DataScience_Projects"
+  },
+  {
+    title: "Covid19 predictions",
+    image: projectImage2,
+    description: `Predictions for Covid-19 spread and impact.`,
+    techstack: "Data Science",
+    githubLink: "https://github.com/Abmstpha/My_DataScience_Projects"
+  },
+  {
+    title: "DataTuring-Kaggle-Challenge",
+    image: projectImage3,
+    description: `Participation in a Kaggle challenge organized by DataTuring.`,
+    techstack: "Data Science",
+    githubLink: "https://github.com/Abmstpha/My_DataScience_Projects"
+  },
+  {
+    title: "Neural Network from Scratch (Only Math)",
+    image: projectImage4,
+    description: `Implementation of a neural network from scratch, focusing on the mathematical concepts.`,
+    techstack: "Data Science",
+    githubLink: "https://github.com/Abmstpha/My_DataScience_Projects"
+  },
+  {
+    title: "ESP-grades-app (Django app)",
+    image: projectImage5,
+    description: `Django web application for managing grades at ESP.`,
+    techstack: "Django",
+    githubLink: "https://github.com/Abmstpha/My_Django_projects"
+  },
+  {
+    title: "Storedatamanagment (Django)",
+    image: projectImage6,
+    description: `Django web application for managing data storage.`,
+    techstack: "Django",
+    githubLink: "https://github.com/Abmstpha/My_Django_projects"
+  }
+];
+
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "abdmstffa@gmail.com",
+  phone: "+222 00000000",
+};
---
+++ src/Pages/About.js
@@ -0,0 +1,74 @@
+import React from "react";
+import Work from "../Components/Work";
+import { personalDetails, workDetails, eduDetails, extraCurricularActivities, achievements } from "../Details";
+
+function About() {
+  return (
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          About Me
+        </h1>
+        <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p>
+      </section>
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Work Experience
+        </h1>
+        {React.Children.toArray(
+          workDetails.map(({ Position, Company, Location, Type, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Education
+        </h1>
+        {React.Children.toArray(
+          eduDetails.map(({ Position, Company, Location, Type, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              type={Type}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Extra-Curricular Activities
+        </h1>
+        <ul className="list-disc ml-8">
+          {extraCurricularActivities.map(({ name, role, duration }) => (
+            <li>
+              <strong>{name}</strong> - {role}, {duration}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Achievements
+        </h1>
+        <ul className="list-disc ml-8">
+          {achievements.map(({ title, description }) => (
+            <li>
+              <strong>{title}</strong> - {description}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default About;
